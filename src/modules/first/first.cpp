fe57902183cb3fa0df0ac77c6abaab80180bc4b7
Sunyi16 <105136305+Sunyi16@users.noreply.github.com>
2023-10-08 10:52:44 +0800

    Add files via upload

--- src/modules/first/first.cpp
+++ src/modules/first/first.cpp
@@ -17,10 +17,6 @@ int First::custom_command(int argc, char *argv[])
 First::	First():
 	ModuleParams(nullptr)
 {
-	/* _actuators_2_pub = nullptr;
-	_actuators_id2 = nullptr;
-	_actuators_2_circuit_breaker_enabled = false;
-	memset(&_actuators2, 0 ,sizeof(_actuators2));//结构体清零 */
 
 }
 First::~First()
@@ -55,87 +51,80 @@ First *First::instantiate(int argc, char *argv[])
 
 void First::run()
 {
-/* 	int ma_co_set_sub_fd = orb_subscribe(ORB_ID(manual_control_setpoint));
-	struct manual_control_setpoint_s mcs;
-	_actuators_id2 =  ORB_ID(actuator_controls_2);
-	while (!PX4_OK)
-	{
-	orb_copy(ORB_ID(manual_control_setpoint) , ma_co_set_sub_fd , &mcs);
-	if((double)mcs.aux2>0.0)
-	{
-	if( (double)mcs.aux1>0.0)
-	{
-	_actuators2.control[4] = -1.0f;//-30
-	_actuators2.control[5] = -1.0f;
-	_actuators2.control[6] = -1.0f;
-	_actuators2.control[7] = -1.0f;
-	}
-	else
-	{
-	_actuators2.control[4] = -0.5f;//1500，对应舵机0度
-	_actuators2.control[5] = -0.5f;
-	_actuators2.control[6] = -0.5f;
-	_actuators2.control[7] = -0.5f;
-	}}
-	else
-	{
-	_actuators2.control[4] = 0.2f;//60
-	_actuators2.control[5] = 0.2f;
-	_actuators2.control[6] = -1.0f;//-30
-	_actuators2.control[7] = -1.0f;
-	}
-	//_actuators2.control[7] = 1.0f;//对应2000us最高值
-	_actuators2.timestamp = hrt_absolute_time();
-	//_actuators2.timestamp_sample = _ctrl_state.timestamp;
-
-
-	if(!_actuators_2_circuit_breaker_enabled){
-		if(_actuators_2_pub!=nullptr){
-			orb_publish(_actuators_id2,_actuators_2_pub,&_actuators2);
-			//PX4_INFO("runing");
-		}else if(_actuators_id2){
-			_actuators_2_pub=orb_advertise(_actuators_id2, &_actuators2);
-		}
-	}
-	} */
 	int succe;
 
 	int uart_read = uart_init((char*)"/dev/ttyS6");
         if(false == uart_read)succe = -1;
         if(false == set_uart_baudrate(uart_read,115200)){
-	printf("%f",(double)succe);
+	printf("12%f",(double)succe);
      //   printf("[YCM]set_uart_baudrate is failed\n");
 
         }
-	while(1){
-	_vehicle_attitude_sub.update(&_v_att);
 
-        float pitch = Eulerf(Quatf(_v_att.q)).theta();
+	while(1)
+	{
+
+	int ser_sub_fd = orb_subscribe(ORB_ID(ser_ver));
+//	bool update;
+//	orb_check(ser_sub_fd, &update);//检查更新
+	orb_copy(ORB_ID(ser_ver),ser_sub_fd,&ser_ver)
+	/*定义六个舵机的控制角度与读取角度信息命令*/
+
+        char con0_write[16] =ser_ver.ser0;//1号舵机，P和T之间为驱动PWM值
+        char con0_write1[10] ="#000PRAD!";//读取对应舵机的角度
+
+	char con1_write[16] =ser_ver.ser1;
+        char con1_write1[10] ="#001PRAD!";
+
+	char con2_write[16] =ser_ver.ser2;
+        char con2_write1[10] ="#002PRAD!";
+
+	char con3_write[16] =ser_ver.ser3;
+        char con3_write1[10] ="#003PRAD!";
+
+	char con4_write[16] =ser_ver.ser4;
+        char con4_write1[10] ="#004PRAD!";
+
+	char con5_write[16] =ser_ver.ser5;
+        char con5_write1[10] ="#005PRAD!";
 
 
-        printf("%f\n",(double)pitch);
+	orb_advert_t ser_pub = orb_advertise(ORB_ID(deg_ser),&deg);
 
-	int mid1=1500+(int)(pitch*1000);
-	char con[5]={'\0'};
-	con[0]=(char)('0'+mid1/1000);
-	con[1]=(char)('0'+mid1%1000/100);
-	con[2]=(char)('0'+mid1%100/10);
-	con[3]=(char)('0'+mid1%10);
+	deg.deg0 = write_read_ser(uart_read , con0_write , con0_write1);
+	deg.deg1 = write_read_ser(uart_read , con1_write , con1_write1);
+	deg.deg2 = write_read_ser(uart_read , con2_write , con2_write1);
+	deg.deg3 = write_read_ser(uart_read , con3_write , con3_write1);
+	deg.deg4 = write_read_ser(uart_read , con4_write , con4_write1);
+	deg.deg5 = write_read_ser(uart_read , con5_write , con5_write1);
 
-
-        char con1_write[16] ="#001P2000T0000!";
-	con1_write[5]=con[0];
-	con1_write[6]=con[1];
-	con1_write[7]=con[2];
-	con1_write[8]=con[3];
-
-	write(uart_read,&con1_write,15);
-	//px4_sleep(0.5);
+	orb_publish(ORB_ID(deg_ser), ser_pub , &deg);
 
 	}
 
 	}
 
+
+int First::write_read_ser(int uart_read ,char *con0_write , char *con0_write1)
+{
+
+	char data = '0';
+	char buffer[30] = "0";
+	write(uart_read,&con0_write,15);
+	usleep(5000);
+	write(uart_read,&con0_write1,9);
+	/*读程序*/
+
+	usleep(5000);
+        for(int i = 0;i <10;i++){
+		read(uart_read,&data,1);
+                buffer[i] = data;
+                data = '0';
+		usleep(5000);}
+	int ser0 = buffer[5]*1000+buffer[6]*100+buffer[7]*10+buffer[8];
+	int ser_deg0 = (int)((ser0-1500)*0.135);
+	return ser_deg0;
+}
 
 
 int First::print_usage(const char *reason )
